707f18999d5a2dbc3c9b7cbab34f9ab69b5f48ca
M Mishal <79745211+Mishal18786@users.noreply.github.com>
2024-09-27 15:11:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,36 +3,20 @@ import pandas as pd
 from sqlalchemy import create_engine
 import oracledb
 
-def load_data(query):
-    user = 'system'
-    password = 'arich'
-    host = 'localhost'
-    port = 1521
-    sid = 'xe'
 
-    dsn = oracledb.makedsn(host, port, sid)
-    conn_str = f'oracle+oracledb://{user}:{password}@{dsn}'
+import json
+import airbyte as ab
+import pandas as pd
+import plotly.graph_objects as go
+import streamlit as st
+from datetime import datetime
+from itertools import islice
+from plotly.subplots import make_subplots
 
-    engine = create_engine(conn_str)
-
-    try:
-        df = pd.read_sql(query, engine)
-        return df
-    except Exception as e:
-        st.error(f"Error loading data: {e}")
-        return pd.DataFrame()  # Return an empty DataFrame on error
-    finally:
-        engine.dispose()  # Ensure the connection is properly closed
 
 # Streamlit app layout
 st.title("🎈 My New App")
 st.write("Loading data from the database...")
 
 # Load data from the Oracle database
-data = load_data('SELECT * FROM cat')
 
-# Display the DataFrame using Streamlit
-if not data.empty:
-    st.write(data)
-else:
-    st.write("No data available or there was an error loading the data.")
